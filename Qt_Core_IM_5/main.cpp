327d05a3a2d456bc76bca67e040d40d167f35893
kimtaehyung <taehyeong77@naver.com>
2019-11-19 19:44:48 +0900

    commit

--- Qt_Core_IM_5/main.cpp
+++ Qt_Core_IM_5/main.cpp
@@ -1,8 +1,55 @@
 #include <QtCore/QCoreApplication>
 
+#include <qlist.h>
+#include <qvector.h>
+#include <qdebug.h>
+
+
+template<class T>
+void fill(T& container) {
+	for (int i = 0; i < 5; i++) {
+		container.append(i);
+	}
+}
+
+template<class T>
+void display(T& container) {
+	for (int i = 0; i < container.length(); i++) {
+		if (i > 0) {
+			auto current = reinterpret_cast<std::uintptr_t>(&container.at(i));
+			auto previous = reinterpret_cast<std::uintptr_t>(&container.at(i - 1));
+
+			auto distance = current - previous;
+
+			qInfo() << i << "At : " << current << "Previous : " << previous << "Distance : " << distance;
+		}
+		else {
+			qInfo() << i << &container.at(i) << "Distance : 0";
+		}
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
 
+	QVector<int> vect = QVector<int>();
+	QList<int> list = QList<int>();
+
+	fill(vect);
+	fill(list);
+
+	qInfo() << "vector";
+	display(vect);
+
+	qInfo() << "list";
+	display(list);
+
+	qInfo() << "Int takes " << sizeof(int) << "bytes of ram";
+
+	// QVector uses continuous location in memory
+	// QList uses what ever it can find in memory
+
 	return a.exec();
 }
+
